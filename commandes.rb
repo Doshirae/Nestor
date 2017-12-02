008f0fb54a6e41743b4ebee6a894177e0eec7a98
Doshirae <doshirae@lilo.org>
2017-12-02 12:28:05 +0100

    Moved strawpoll command to commandes.rb

--- commandes.rb
+++ commandes.rb
@@ -1,19 +1,41 @@
 require_relative "insultotron.rb"
+require "httparty"
+
 def random(*args)
-	if args[1]
-		max = args[1].to_i
-		min = args[0].to_i
-	elsif args[0]
-		max = args[0].to_i
-		min = 0
-	else
-		max = 1.to_f
-		min = 0.to_f
-	end
-	rand(min..max)
+  if args[1]
+    max = args[1].to_i
+    min = args[0].to_i
+  elsif args[0]
+    max = args[0].to_i
+    min = 0
+  else
+    max = 1.to_f
+    min = 0.to_f
+  end
+  rand(min..max)
 end
 
 
 def insulte(mode = nil)
-	"#{Insulte.new(mode)}"
+  "#{Insulte.new(mode)}"
+end
+
+
+def strawpoll(name, *choices)
+  if choices.empty?
+    return "Mais enfin ! Quelle curieuse idée de faire un sondage sans le moindre choix... On se croirait en Russie"
+  else
+    choices = choices.join(' ').split(' | ')
+    multi = false
+    if choices[0] =~ /q\s.*/
+      question = choices.shift[2..-1]
+    elsif choices[0] =~ /qm\s.*/
+      question = choices.shift[2..-1]
+      multi = true
+    end
+    question ||= "Poll de #{name}"
+    json = HTTParty.post("https://strawpoll.me/api/v2/polls", body: {title: "#{question}", options: choices, multi: multi}.to_json).body
+    id = JSON.parse(json)["id"]
+    return "Voici pour vous, mon cher\nhttps://strawpoll.me/#{id}"
+  end
 end
