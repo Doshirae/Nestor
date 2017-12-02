d38512eaf71d2b8d0262bfe359ff62ff63d6f853
Doshirae <doshirae@lilo.org>
2017-12-02 12:37:18 +0100

    Changed timers of asterix sentences

--- Nestor.rb
+++ Nestor.rb
@@ -189,16 +189,17 @@ end
 
 bot.message(containing: /C'est une bonne situation ça,? scribe \?/) do |event|
   event.respond "Vous savez, moi je ne crois pas qu’il y ait de bonne ou de mauvaise situation."
-  sleep 1
+  sleep 1.5
   event.respond "Moi, si je devais résumer ma vie aujourd’hui avec vous, je dirais que c’est d’abord des rencontres."
-  sleep 1
+  sleep 3
   event.respond "Des gens qui m’ont tendu la main, peut-être à un moment où je ne pouvais pas, où j’étais seul chez moi."
-  sleep 1
+  sleep 2
   event.respond "Et c’est assez curieux de se dire que les hasards, les rencontres forgent une destinée…"
-  sleep 1
+  sleep 1.5
   event.respond "Parce que quand on a le goût de la chose, quand on a le goût de la chose bien faite, le beau geste, parfois on ne trouve pas l’interlocuteur en face je dirais, le miroir qui vous aide à avancer."
-  sleep 1
+  sleep 4
   event.respond "Alors ça n’est pas mon cas, comme je disais là, puisque moi au contraire, j’ai pu : et je dis merci à la vie, je lui dis merci, je chante la vie, je danse la vie… je ne suis qu’amour !"
+  sleep 4
   event.respond "Et finalement, quand beaucoup de gens aujourd’hui me disent « Mais comment fais-tu pour avoir cette humanité ? », et bien je leur réponds très simplement, je leur dis que c’est ce goût de l’amour ce goût donc qui m’a poussé aujourd’hui à entreprendre une construction mécanique, mais demain qui sait ? Peut-être simplement à me mettre au service de la communauté, à faire le don, le don de soi…"
 end
 # <==
